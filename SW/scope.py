2a3c0d816a1692a1b7fb4f9584dd902635663ec9
Paulo Matias <paulo.matias@usp.br>
2015-06-06 15:39:21 -0300

    Scope improvements

--- SW/scope.py
+++ SW/scope.py
@@ -3,29 +3,41 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import itertools
 
+MaxSample = 0xfff
+MaxVoltage = 2.5       # V
+SampleBits = 12        # bits
+NumWords = 1024        # words
+SamplesPerWord = 5     # samples
+WordBytes = 8          # bytes
+SamplingRate = 50.e3   # Hz
+RenderInterval = 10    # ms
+
 def getSamples(buf):
-    return np.array([(long(x)>>(12*i))&0xfff
+    return np.array([(long(x)>>(SampleBits*i))&MaxSample
                      for x in np.frombuffer(buf, dtype=np.uint64)
-                     for i in xrange(5)], dtype=np.float)
+                     for i in xrange(SamplesPerWord)], dtype=np.float)
 
 dev = open('/dev/rtf0', 'rb')
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(subplot_kw={'axisbg': 'black'})
 
-x = np.linspace(0, 1, 5*1024)
-line, = ax.plot(x, np.zeros(5*1024), 'k.-')
-ax.axis([0, 1, 0, 2.5])
+maxTime = SamplesPerWord*NumWords/SamplingRate
+totalSamples = SamplesPerWord*NumWords
+
+x = np.linspace(0, maxTime, totalSamples)
+line, = ax.plot(x, np.zeros(totalSamples), c='#00ff00', ls='-', marker='.', ms=2.5)
+ax.axis([0, maxTime, 0, MaxVoltage])
 
 def animate(i):
-    line.set_ydata(2.5*getSamples(dev.read(8*1024))/0xfff)
+    line.set_ydata(MaxVoltage*getSamples(dev.read(WordBytes*NumWords))/MaxSample)
     return line,
 
 #Init only required for blitting to give a clean slate.
 def init():
-    line.set_ydata(np.ma.array(np.zeros(5*1024), mask=True))
+    line.set_ydata(np.ma.array(np.zeros(totalSamples), mask=True))
     return line,
 
 ani = animation.FuncAnimation(fig, animate, itertools.cycle([0]), init_func=init,
-    interval=50, blit=True)
+    interval=RenderInterval, blit=True)
 plt.show()
 
