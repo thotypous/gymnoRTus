81820cb1c93851b86ef179d49ed93ee69a132a33
Paulo Matias <paulo.matias@usp.br>
2015-10-16 19:40:36 -0300

    Fix identation

--- SW/recog/svm.h
+++ SW/recog/svm.h
@@ -8,42 +8,42 @@
 #include "svm_model.h"
 
 static inline void svm_prepare_features(float dtcwpt[static WaveletOutSize], double features[static NumFeatures]) {
-	filter_features(dtcwpt, features);
-	add_double4_arr(features, rescaling_offset, NumFeatures);
-	mult_double4_arr(features, rescaling_factor, NumFeatures);
+    filter_features(dtcwpt, features);
+    add_double4_arr(features, rescaling_offset, NumFeatures);
+    mult_double4_arr(features, rescaling_factor, NumFeatures);
 }
 
 static inline double svm_decision_value(double features[static NumFeatures]) {
-	double ALIGN(32) dotp[svm_l + 4];
-	for (int i = 0; i < svm_l; i++)
-		dotp[i] = dot_double4_arr(&svm_SV[i][0], features, NumFeatures);
+    double ALIGN(32) dotp[svm_l + 4];
+    for (int i = 0; i < svm_l; i++)
+        dotp[i] = dot_double4_arr(&svm_SV[i][0], features, NumFeatures);
 
-	__m256d* dotp4 = (__m256d*)dotp;
-	__m256d* sv_coef4 = (__m256d*)svm_sv_coef;
-	__m256d sum4={};
-	for (int i = 0; i < (svm_l + 4)/4; i++)
-		sum4 += sv_coef4[i] * exp_double4(-svm_gamma * dotp4[i]);
+    __m256d* dotp4 = (__m256d*)dotp;
+    __m256d* sv_coef4 = (__m256d*)svm_sv_coef;
+    __m256d sum4={};
+    for (int i = 0; i < (svm_l + 4)/4; i++)
+        sum4 += sv_coef4[i] * exp_double4(-svm_gamma * dotp4[i]);
 
-	return sum4[0] + sum4[1] + sum4[2] + sum4[3] - svm_rho;
+    return sum4[0] + sum4[1] + sum4[2] + sum4[3] - svm_rho;
 }
 
 // based on libsvm
 static inline double sigmoid_predict(const double decision_value)
 {
-	const double fApB = svm_probA * decision_value + svm_probB;
-	double prob;
+    const double fApB = svm_probA * decision_value + svm_probB;
+    double prob;
 
-	if (fApB >= 0) {
-		const double exp_minus_fApB = scalar_exp(-fApB);
-		prob = exp_minus_fApB/(1.0+exp_minus_fApB);
-	}
-	else {
-		const double exp_fApB = scalar_exp(fApB);
-		prob = 1.0/(1.0+exp_fApB) ;
-	}
+    if (fApB >= 0) {
+        const double exp_minus_fApB = scalar_exp(-fApB);
+        prob = exp_minus_fApB/(1.0+exp_minus_fApB);
+    }
+    else {
+        const double exp_fApB = scalar_exp(fApB);
+        prob = 1.0/(1.0+exp_fApB) ;
+    }
 
-	const double min_prob = 1e-7;
-	return __builtin_fmin(1-min_prob, __builtin_fmax(min_prob, prob));
+    const double min_prob = 1e-7;
+    return __builtin_fmin(1-min_prob, __builtin_fmax(min_prob, prob));
 }
 
 #endif
